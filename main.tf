3d73f76b13f867c132a4eb2cb51271c5ee1395b5
riko rakhmanto <riko.rakhmanto@rackspace.com>
2020-03-11 12:16:00 +1100

    add simple terraform

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  version             = "~> 2.17"
+  region              = "ap-southeast-2"
+}
+
+provider "random" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}
+
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
